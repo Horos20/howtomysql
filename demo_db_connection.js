af688ce30c68036849bba5ced6a236b1feaa333d
Matis Rammi <matis.rammi@khk.ee>
2022-02-12 18:17:53 +0200

    Select from table

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -9,7 +9,7 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
     if (err) throw err;
-    con.query("SELECT * FROM customers", function (err, result, fields) {
+    con.query("SELECT * FROM customers", function (err, result) {
         if (err) throw err;
         console.log(result);
     });
